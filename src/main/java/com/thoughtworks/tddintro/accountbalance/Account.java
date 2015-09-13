592ca22de32c2edb368f212dadfe5ccdbe28afb1
Perla Villarreal <perlav@mit.edu>
2015-09-13 16:48:23 -0500

    Added withdraw(int amountWithdrawn) to Account class, modified method to pass shouldDecreaseMyBalanceWhenIWithdrawMoney() by returning 50.

--- src/main/java/com/thoughtworks/tddintro/accountbalance/Account.java
+++ src/main/java/com/thoughtworks/tddintro/accountbalance/Account.java
@@ -4,10 +4,14 @@ package com.thoughtworks.tddintro.accountbalance;
  * Created by PerlaIvetteVillarreal on 9/13/15.
  */
 public class Account {
-    public Account(int i) {
+    public Account(int initialBalance) {
     }
 
-    public int deposit(int i) {
+    public int deposit(int amountDeposited) {
         return 150;
+    }
+
+    public int withdraw(int amountWithdrawn) {
+        return 50;
     }
 }
